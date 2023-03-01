5aefb51136f1a37c7c34bcd82f38a7a2d55bda1e
Marco Silva <penajoia.marco@gmail.com>
2023-03-01 15:09:43 -0800

    switch whole url to just playlistid in fetch

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from 'react'
 
-function useFetch(url) {
+function useFetch(playlistId) {
     const [playlist, setPlaylist] = useState(null)
-    const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
 
     useEffect(()=> {
-        
-    },[])
+        axios.get(`https://cdn.jwplayer.com/v2/playlists/${playlistId}`)
+        .then((response) => {
+            console.log(response.data)
+            setPlaylist(response.data)
+        })
+        .catch((e) => setError(e))
+    },[url])
 
-  return { playlist, loading, error }
+  return { playlist, error }
 }
 
 export default useFetch
