9717e99d453d3e43e0a6076d333f0017c630e18f
Marco Silva <penajoia.marco@gmail.com>
2023-03-06 12:42:16 -0800

    minute tweaks

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Sidebar() {
   return (
     <div className='sidebar'>
-      <p>Input a Playlist ID such as lDF3B0qj or wZDkcC5x</p>
+      <p>Input a Playlist ID such as wZDkcC5x or lDF3B0qj</p>
     </div>
   )
 }
